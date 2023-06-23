b4356e185c296b4e61b0566a0bd96d012ba9c7fe
partouf <partouf@gmail.com>
2023-06-23 02:04:33 +0200

    remove more demo stuff

--- src/host/webFrontEnd/loaderDialog.js
+++ src/host/webFrontEnd/loaderDialog.js
@@ -1,15 +1,10 @@
-import hoverBovverPrg from "./demos/hoverbovver_prg";
-import firstPrg       from "./demos/first_prg";
-
 import { closeAllDialogs } from "./dialogs";
 import { ingest } from "../ingest";
-import css from "./loaderDialog.css";
 
 let c64;
 
 export function initLoaderDialog(nascentC64) {
   c64 = nascentC64;
-
 
 }
 
